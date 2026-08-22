63920bb41602f10dc2c48f43978ce3d4b29d1ce7
Seongho Bae <me@seonghobae.me>
2026-08-22 00:09:51 -0700

    chore(readiness): remove unrelated database comment drift

--- config/database.php
+++ config/database.php
@@ -152,9 +152,9 @@ return [
     | Migration Repository Table
     |--------------------------------------------------------------------------
     |
-    | This table keeps track of all of the migrations that have already run for
-    | your application. Using this information, we can determine which of them
-    | have not actually run.
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run on the database.
     |
     */
 
